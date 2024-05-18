6551f1a2a6885c8469a6c76b171570c15d8359cd
Alif Faidhi Ifwat Ma'min <faidhifwat@gmail.com>
2024-05-19 00:29:08 +0800

    first commit

---
+++ todo/src/main/java/com/sultan/vizier/TodoApplication.java
@@ -0,0 +1,21 @@
+package com.sultan.vizier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;  
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@SpringBootApplication
+public class TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World!";
+	}
+
+}
---
+++ todo/src/test/java/com/sultan/vizier/VizierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sultan.vizier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VizierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
